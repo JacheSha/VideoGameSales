e2429958f80ebc055665ab4291fe3b49ec3f098f
JacheSha <150440581+JacheSha@users.noreply.github.com>
2024-11-07 12:52:36 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,82 @@
+const GameTypeData = [
+    { name: "Action", nameChinese: "动作", sales: 3251 },
+    { name: "Sports", nameChinese: "体育", sales: 2304 },
+    { name: "Misc", nameChinese: "杂项", sales: 1686 },
+    { name: "Role-Playing", nameChinese: "角色扮演", sales: 1470 },
+    { name: "Shooter", nameChinese: "射击", sales: 1282 },
+    { name: "Adventure", nameChinese: "冒险", sales: 1274 },
+    { name: "Racing", nameChinese: "赛车", sales: 1225 },
+    { name: "Platform", nameChinese: "平台", sales: 875 },
+    { name: "Simulation", nameChinese: "模拟", sales: 848 },
+    { name: "Fighting", nameChinese: "格斗", sales: 836 },
+    { name: "Strategy", nameChinese: "策略", sales: 670 },
+    { name: "Puzzle", nameChinese: "益智", sales: 570 }
+];
+
+const GameTypeSales = [
+    { name: "Action", nameChinese: "动作", sales: 861.77 },
+    { name: "Sports", nameChinese: "体育", sales: 670.09 },
+    { name: "Shooter", nameChinese: "射击", sales: 575.16 },
+    { name: "Platform", nameChinese: "平台", sales: 445.99 },
+    { name: "Misc", nameChinese: "其他", sales: 396.92 },
+    { name: "Racing", nameChinese: "赛车", sales: 356.93 },
+    { name: "Role-Playing", nameChinese: "角色扮演", sales: 326.50 },
+    { name: "Fighting", nameChinese: "格斗", sales: 220.74 },
+    { name: "Simulation", nameChinese: "模拟", sales: 181.78 },
+    { name: "Puzzle", nameChinese: "解密", sales: 122.01 },
+    { name: "Adventure", nameChinese: "冒险", sales: 101.93 },
+    { name: "Strategy", nameChinese: "策略", sales: 67.83 }
+];
+
+//历年总销量
+const sales_by_year = [
+    { year: 1980, sales: 11.38 },
+    { year: 1981, sales: 35.77 },
+    { year: 1982, sales: 28.86 },
+    { year: 1983, sales: 16.79 },
+    { year: 1984, sales: 50.36 },
+    { year: 1985, sales: 53.94 },
+    { year: 1986, sales: 37.07 },
+    { year: 1987, sales: 21.74 },
+    { year: 1988, sales: 47.22 },
+    { year: 1989, sales: 73.45 },
+    { year: 1990, sales: 49.39 },
+    { year: 1991, sales: 32.23 },
+    { year: 1992, sales: 76.16 },
+    { year: 1993, sales: 45.98 },
+    { year: 1994, sales: 79.17 },
+    { year: 1995, sales: 88.11 },
+    { year: 1996, sales: 199.15 },
+    { year: 1997, sales: 200.98 },
+    { year: 1998, sales: 256.47 },
+    { year: 1999, sales: 251.27 },
+    { year: 2000, sales: 201.56 },
+    { year: 2001, sales: 331.47 },
+    { year: 2002, sales: 395.52 },
+    { year: 2003, sales: 357.85 },
+    { year: 2004, sales: 414.01 },
+    { year: 2005, sales: 458.51 },
+    { year: 2006, sales: 521.04 },
+    { year: 2007, sales: 609.92 },
+    { year: 2008, sales: 678.90 },
+    { year: 2009, sales: 667.30 },
+    { year: 2010, sales: 600.29 },
+    { year: 2011, sales: 515.80 },
+    { year: 2012, sales: 363.49 },
+    { year: 2013, sales: 368.11 },
+    { year: 2014, sales: 337.03 },
+    { year: 2015, sales: 264.44 },
+    { year: 2016, sales: 70.90 },
+    { year: 2017, sales: 0.05 },
+    { year: 2020, sales: 0.29 }
+];
+//总销量
+const const_total_sales = {
+    NA_Sales: 4327.65,
+    EU_Sales: 2406.69,
+    JP_Sales: 1284.27,
+    Other_Sales: 788.91
+  };
+
+  
+  
